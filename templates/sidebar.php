f1caddef48db54a303e42f0119ae5e74b6e76e0f
deryoid <dery037yj@gmail.com>
2021-12-03 21:12:59 +0800

    ok

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -98,7 +98,7 @@
             </a>
           </li>
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
+            <a href="" class="nav-link">
               <i class="nav-icon fas fa-file"></i>
               <p>
                 Laporan
